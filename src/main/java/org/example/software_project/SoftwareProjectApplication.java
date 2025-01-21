8a5598da150cb4a2ce364c4d56db9a544f8b1b8d
Chealy-13 <chris.healy33@gmail.com>
2025-01-21 11:29:47 +0000

    Initial commit

---
+++ src/main/java/org/example/software_project/SoftwareProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.software_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/software_project/business/starter_test_file.java
@@ -0,0 +1,6 @@
+package org.example.software_project.business;
+
+public class starter_test_file {
+
+    //This is a test
+}
